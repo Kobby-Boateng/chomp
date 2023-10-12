ffc75635bb77bb10f0d68e11ea1d9681d015effb
Kobby-Boateng <m260564@usna.edu>
2023-10-12 09:45:10 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,2 +1,21 @@
-print ("hello world!") 
-print("message!")
+import pygame
+import time
+
+print(f"the quit event is type {pygame.QUIT}")
+pygame.init()
+
+screen = pygame.display.set_mode((400,400))
+pygame.display.set_caption("chomp!!!")
+
+
+screen.fill((100,53,255))
+pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+
+pygame.display.flip()
+
+while True:
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            print ("you're stuck here forever")
+
