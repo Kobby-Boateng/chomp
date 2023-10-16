b9284351e335940ff5a40bc2bceea7c1c22b1bc9
Kobby-Boateng <m260564@usna.edu>
2023-10-16 09:37:09 -0400

    Update game.py

--- game.py
+++ game.py
@@ -12,12 +12,11 @@ pygame.display.set_caption("chomp!!!")
 
 screen.fill((100,53,255))
 pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
-
+sand = pygame.image.load("assets/images/sand.png").convert()
+screen.blit(sand,(200,200,64,64))
 pygame.display.flip()
-
 while True:
     for event in pygame.event.get():
-
 
         if event.type == pygame.QUIT:
             print ("Thanks for Playing ")
