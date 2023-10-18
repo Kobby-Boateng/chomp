a8134a0ab0676f0ee79f962fb2f18a127595165c
Kobby-Boateng <m260564@usna.edu>
2023-10-18 09:27:28 -0400

    Update game.py
    We made numerical values parametric so they aren't "fixed" values but proportionally changing values based off variables

--- game.py
+++ game.py
@@ -1,25 +1,34 @@
 import sys
-
 import pygame
-import time
 
-print(f"the quit event is type {pygame.QUIT}")
+# dimensions
+SCREEN_WIDTH = 600
+SCREEN_HEIGHT = 400
+SAND_HEIGHT = 20
+TILE_SIZE = 64  # tile are square height == width
+
+# colors
+WATER_COLOR = (100, 53, 255)
+SAND_COLOR = (100, 25, 0)
 pygame.init()
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("chomp!!!")
-
-
-screen.fill((100,53,255))
-pygame.draw.rect(screen, (100, 25, 0), (0, 380, 400, 400))
+screen.fill(WATER_COLOR)
+pygame.draw.rect(screen, SAND_COLOR, (0,
+                                      SCREEN_HEIGHT - SAND_HEIGHT,
+                                      SCREEN_WIDTH,
+                                      SAND_HEIGHT))
 sand = pygame.image.load("assets/images/sand.png").convert()
-screen.blit(sand,(200,200,64,64))
+screen.blit(sand, (SCREEN_WIDTH/2 - TILE_SIZE/2,
+                   SCREEN_HEIGHT/2 - TILE_SIZE/2))
 pygame.display.flip()
 while True:
     for event in pygame.event.get():
 
         if event.type == pygame.QUIT:
-            print ("Thanks for Playing ")
+            print("Thanks for Playing ")
             pygame.quit()
             sys.exit()
 
+        pygame.display.flip()
